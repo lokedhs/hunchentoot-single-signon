42a3abc56cfc0bfb436bea68f8cc403cb7af6aa7
Elias Mårtenson <lokedhs@gmail.com>
2013-10-31 12:35:26 +0800

    Added a docstring to the SPNEGO-AUTH function

--- src/spnego.lisp
+++ src/spnego.lisp
@@ -1,6 +1,21 @@
 (in-package :hunchentoot-single-signon)
 
 (defun spnego-auth (body-handler-fn &optional failed-auth-fn)
+  "Performs SPNEGO authentication. This function is intended to be
+called from a Hunchentoot handler, and the implementation assumes that
+no further Hunchentoot-related processing will take place after this
+call.
+
+BODY-HANDLER-FN is a function that will be called after successful
+authentication. It is called with an instance of CL-GSS:NAME
+representing the identify of the user.
+
+If given, FAILED-AUTH-FN is a function that will be called when
+authentication fails. Note that before this function is called, the
+return code has already been set to the correct value and should not
+be changed. The purpose of this function is to provide a more useful
+error message, and also possibly display a normal login and password
+form to provide some alternate login method."
   (labels ((failed-auth ()
              (setf (hunchentoot:return-code*) hunchentoot:+http-authorization-required+)
              (if failed-auth-fn
